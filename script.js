07353544519dbf10f9a6df2d55869353ac8dbcb3
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-10-27 20:03:53 +0000

    slider completed

--- script.js
+++ script.js
@@ -23,24 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
 // Swiper
 
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+  breakpoints: {
+    768: {
+      pagination: false,
+    },
   },
 });
 
