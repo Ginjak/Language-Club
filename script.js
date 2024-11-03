7f6837cf54cea5bb8aded89692306e536cccc131
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-11-03 09:59:17 +0000

    registration page completed

--- script.js
+++ script.js
@@ -1,122 +1,127 @@
-// Mobile menu toggle
-
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".hamburger-menu-toggle-wraper");
   const bottomNav = document.getElementById("bottom-nav");
   const closeButton = document.querySelector(".close");
 
   // Apply initial transition style to `bottomNav`
-  bottomNav.style.transition = "top 0.3s linear";
-  bottomNav.style.top = "-100vh"; // Start hidden
+  if (bottomNav) {
+    bottomNav.style.transition = "top 0.3s linear";
+    bottomNav.style.top = "-100vh"; // Start hidden
 
-  // Show navigation
-  menuToggle.addEventListener("click", () => {
-    bottomNav.style.top = "0";
-  });
+    // Show navigation
+    if (menuToggle) {
+      menuToggle.addEventListener("click", () => {
+        bottomNav.style.top = "0";
+      });
+    }
 
-  // Hide navigation
-  closeButton.addEventListener("click", () => {
-    bottomNav.style.top = "-100vh";
-  });
-});
+    // Hide navigation
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        bottomNav.style.top = "-100vh";
+      });
+    }
+  }
 
-// Swiper
-if (typeof Swiper !== "undefined") {
-  const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    loop: true,
+  // Swiper
+  if (typeof Swiper !== "undefined") {
+    const swiper = new Swiper(".swiper", {
+      direction: "horizontal",
+      loop: true,
 
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      768: {
-        pagination: false,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-    },
-  });
 
-  var brandSwiper = new Swiper("#brands", {
-    loop: true,
-    slidesPerView: 7,
-    spaceBetween: 30,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        768: {
+          pagination: false,
+        },
+      },
+    });
 
-    breakpoints: {
-      320: {
-        slidesPerView: 3,
-        spaceBetween: 20,
+    var brandSwiper = new Swiper("#brands", {
+      loop: true,
+      slidesPerView: 7,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
       },
-      480: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-      768: {
-        slidesPerView: 5,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 7,
-        spaceBetween: 30,
-      },
-    },
-  });
-}
 
-// Kalbu pasirinkimas
-document.addEventListener("DOMContentLoaded", function () {
+      breakpoints: {
+        320: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        480: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 7,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+
+  // Kalbu pasirinkimas
   const selectLng = document.getElementById("select-lng");
   const languageEnglish = document.querySelector(".language-english");
   const languageWrapper = document.querySelector(".language-wraper");
 
-  selectLng.addEventListener("click", function () {
-    languageEnglish.classList.toggle("d-none");
-    languageEnglish.classList.toggle("d-block");
-  });
+  if (selectLng && languageEnglish && languageWrapper) {
+    selectLng.addEventListener("click", function () {
+      languageEnglish.classList.toggle("d-none");
+      languageEnglish.classList.toggle("d-block");
+    });
 
-  languageWrapper.addEventListener("mouseleave", function () {
-    if (languageEnglish.classList.contains("d-block")) {
-      languageEnglish.classList.remove("d-block");
-      languageEnglish.classList.add("d-none");
-    }
-  });
-
-  languageWrapper.querySelectorAll("a").forEach(function (link) {
-    link.addEventListener("click", function () {
+    languageWrapper.addEventListener("mouseleave", function () {
       if (languageEnglish.classList.contains("d-block")) {
         languageEnglish.classList.remove("d-block");
         languageEnglish.classList.add("d-none");
       }
     });
-  });
-});
 
-// Footer current year
-function displayCurrentYear() {
-  const currentYear = new Date().getFullYear(); // Get the current year
-  const elements = document.querySelectorAll(".current-year"); // Select all elements with the class 'current-year'
+    languageWrapper.querySelectorAll("a").forEach(function (link) {
+      link.addEventListener("click", function () {
+        if (languageEnglish.classList.contains("d-block")) {
+          languageEnglish.classList.remove("d-block");
+          languageEnglish.classList.add("d-none");
+        }
+      });
+    });
+  }
 
-  // Update the text content of each selected element
-  elements.forEach((element) => {
-    element.textContent = currentYear;
-  });
-}
-displayCurrentYear();
+  // Footer current year
+  function displayCurrentYear() {
+    const currentYear = new Date().getFullYear(); // Get the current year
+    const elements = document.querySelectorAll(".current-year"); // Select all elements with the class 'current-year'
 
-// Textarea expansion
+    // Update the text content of each selected element
+    elements.forEach((element) => {
+      element.textContent = currentYear;
+    });
+  }
+  displayCurrentYear();
 
-const textarea = document.getElementById("pastabos");
+  // Textarea expansion
+  const textarea = document.getElementById("pastabos");
 
-textarea.addEventListener("input", () => {
-  textarea.style.height = "auto";
-  textarea.style.height = `${textarea.scrollHeight}px`;
+  if (textarea) {
+    textarea.addEventListener("input", () => {
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    });
+  }
 });
